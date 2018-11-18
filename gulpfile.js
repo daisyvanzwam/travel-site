c06ef3a978e896bb607e17e2ba3dd4258fad9b88
Daisy <info@vanzwammedia.nl>
2018-11-18 14:08:38 +0100

    Gulp added

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+
+gulp.task('default', function() {
+    console.log("Hi");
+});
+
+gulp.task('html', function() {
+    console.log('Something usefull');
+});
+
+gulp.task('styles', function() {
+    console.log('Something styles');
+});
+
+gulp.task('watch', function() {
+    watch('./app/index.html', function() {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('styles');
+    });
+});
