6504822a6176aa75ce218c94452d542c882fb55f
Daisy <info@vanzwammedia.nl>
2018-11-19 16:50:19 +0100

    Browsersync added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
     console.log("Hi");
@@ -21,11 +22,22 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }
+    });
+
     watch('./app/index.html', function() {
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css', function() {
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject', ['styles'], function() {
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
 });
